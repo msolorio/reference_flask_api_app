66c744dd2e857e20282ae93e37c8f65cb6ee1c23
msolorio <msolorio.github@gmail.com>
2022-01-07 10:47:03 -0800

    Cleans up code

--- app/app.py
+++ app/app.py
@@ -1,15 +1,13 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, redirect, url_for
 from flask_migrate import Migrate
 from .models import db, ma
 from .controllers.api_bp import api_bp
 
 def create_app():
-    app = Flask(__name__)
-
+    app = Flask(__name__)   
     app.config.from_object('app.config')
     db.init_app(app)
     ma.init_app(app)
-
     Migrate(app, db)
 
     app.register_blueprint(api_bp, url_prefix='/api')
@@ -18,5 +16,4 @@ def create_app():
     def index():
         return redirect(url_for('api_root'))
     
-
     return app
--- app/controllers/api_bp.py
+++ app/controllers/api_bp.py
@@ -4,6 +4,8 @@ from .autos_bp import autos_bp
 # current route - /api
 api_bp = Blueprint('api_bp', __name__)
 
+api_bp.register_blueprint(autos_bp, url_prefix='/automobiles')
+
 @api_bp.route('/')
 def api_root():
         return { 'message': 'Welcome to the reference Flask API. Navigate to /api/docs to access OpenAPI docs.' }
@@ -13,5 +15,3 @@ def api_root():
 def get_docs():
     print('sending docs')
     return render_template('swaggerui.html')
-
-api_bp.register_blueprint(autos_bp, url_prefix='/automobiles')

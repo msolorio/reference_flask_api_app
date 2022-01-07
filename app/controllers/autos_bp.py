db8cdb18338287c3bf2fb7e22d0c915acdc7492b
msolorio <msolorio.github@gmail.com>
2022-01-06 16:33:53 -0800

    Adds full crud for autos

--- app/controllers/autos_bp.py
+++ app/controllers/autos_bp.py
@@ -9,6 +9,18 @@ def index():
     return jsonify(autos_schema.dump(autos))
 
 
+
+@autos_bp.route('/<int:auto_id>')
+def show(auto_id):
+    auto = Automobile.query.get(auto_id)
+
+    if not auto:
+        return { 'message': 'No automobile with that id' }, 404
+
+    return auto_schema.dump(auto)
+
+
+
 @autos_bp.route('/', methods=['POST'])
 def create():
     json_data = request.json
@@ -29,3 +41,31 @@ def create():
     db.session.commit()
 
     return auto_schema.dump(new_auto)
+
+
+
+@autos_bp.route('/<int:auto_id>', methods=['PATCH'])
+def update(auto_id):
+    auto = Automobile.query.get(auto_id)
+    json_data = request.json
+    available_fields = ('make', 'model', 'color', 'year')
+
+    if not auto:
+        return { 'message': 'No automobile with that id' }, 404
+
+    for field in available_fields:
+        if field in json_data:
+            setattr(auto, field, json_data[field])
+
+    db.session.commit()
+    return auto_schema.dump(auto)
+
+
+@autos_bp.route('/<int:auto_id>', methods=['DELETE'])
+def delete(auto_id):
+    auto = Automobile.query.get(auto_id)
+
+    db.session.delete(auto)
+    db.session.commit()
+
+    return '', 204
